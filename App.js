00238588d5a6684d0c26c8ddf38925d893b66aca
kynguyenvn <kynguyen92bkdn@gmail.com>
2022-12-08 15:27:08 +0700

    feat: add react-navigation

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { withExpoSnack } from 'nativewind';
+import * as React from "react";
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { withExpoSnack } from "nativewind";
 
-import { Text, View } from 'react-native';
-import { styled } from 'nativewind';
-
-const StyledView = styled(View)
-const StyledText = styled(Text)
-
-const App = () => {
+function HomeScreen({ navigation }) {
   return (
-<View className="flex-1 items-center justify-center">
-      <Text className="text-fuchsia-200">Styling just works! 🎉</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-500">Home just works! 🎉</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
     </View>
   );
 }
 
-// This demo is using a external compiler that will only work in Expo Snacks.
-// You may see flashes of unstyled content, this will not occur under normal use!
-// Please see the documentation to setup your application
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button title="Go to Details" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 export default withExpoSnack(App);
