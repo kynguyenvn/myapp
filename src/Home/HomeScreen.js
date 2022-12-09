ad0f3f6f6f46119c18e6542d5a5507ec68752c2a
kynguyenvn <kynguyen92bkdn@gmail.com>
2022-12-09 17:19:17 +0700

    feat: build ui home

--- src/Home/HomeScreen.js
+++ src/Home/HomeScreen.js
@@ -1,6 +1,19 @@
 import React, { useLayoutEffect } from "react";
-import { View, Text, Button, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  Image,
+  TextInput,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import {
+  ChevronDownIcon,
+  UserIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -11,26 +24,38 @@ const HomeScreen = () => {
     });
   }, []);
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-red-500">Home src just works! 🎉</Text>
-      <Button
-        title="Go to Detail"
-        onPress={() => navigation.navigate("Details")}
-      />
-      <Button
-        title="Go to Home2"
-        onPress={() => navigation.navigate("Home2")}
-      />
-      <View>
-        <Text className="text-red-500">Here an images! 🎉</Text>
+    <SafeAreaView className="bg-white pt-5">
+      {/* header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
           source={{
             uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJgMhcfdWNRQCCckkiYhyYnPbAm8_ZFGajFQ&usqp=CAU",
           }}
-          style={{ height: 50, width: 50 }}
+          style={{ height: 30, width: 30 }}
+          className="w-50 h-50 p4 rounded-full bg-blue-600"
         />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
       </View>
-    </View>
+      {/* search */}
+
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon size={20} color="#00CCBB" />
+      </View>
+    </SafeAreaView>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
     "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
